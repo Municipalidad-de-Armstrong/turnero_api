30ed9e530d34bb6d15a662296abd1f9e07415847
Pablo Ingegnieri <pabloingegnieri1@gmail.com>
2026-07-30 02:23:36 -0300

    fix(schema): agregar server_default y default de fecha/hora en entidad turnos

--- app/models/turno.py
+++ app/models/turno.py
@@ -71,10 +71,14 @@ class Turno(Base):
         Text, nullable=True
     )
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), nullable=False
+        DateTime(timezone=True),
+        default=func.now(),
+        server_default=func.now(),
+        nullable=False,
     )
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
+        default=func.now(),
         server_default=func.now(),
         onupdate=func.now(),
         nullable=False,
